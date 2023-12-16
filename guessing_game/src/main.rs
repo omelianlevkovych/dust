e2d9cce70432e7f1da60e9dfe058917cb28342d5
Omelian Levkovych <omelianai@gmail.com>
2023-12-16 21:47:04 +0100

    Simple guessing game

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,17 @@
+use std::io;
+
+
+fn main() {
+    println!("Guess the number!");
+    
+    println!("Please input your guess.");
+
+    let constant = 5; // immutable
+    let mut guess = String::new(); // mutable
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line.");
+
+    println!("You guessed: {}", guess);
+}
