89fdc94bd139ced23107fc5cb538a62fb25aedcf
Omelian Levkovych <omelianai@gmail.com>
2023-12-19 08:23:25 +0100

    Statements and expressions

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
     println!("Hello, world!");
-    print_something(10, 'm');
+    print_something(10);
 }
 
-fn print_something(x: i32, unit_label: char) {
-    println!("The value of x is: {x}{unit_label}");
+fn print_something(x: i32) {
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {y}");
 }
+
