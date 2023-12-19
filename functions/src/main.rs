fdb952ba46e3eccc1e224e050746295216e94bc5
Omelian Levkovych <omelianai@gmail.com>
2023-12-19 08:19:52 +0100

    Function with params

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     println!("Hello, world!");
-    print_something();
+    print_something(10, 'm');
 }
 
-fn print_something() {
-    println!("Something");
+fn print_something(x: i32, unit_label: char) {
+    println!("The value of x is: {x}{unit_label}");
 }
