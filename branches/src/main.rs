f857bed12bb2587fd974419e0cdab2481e431203
Omelian Levkovych <omelianai@gmail.com>
2023-12-19 20:02:04 +0100

    Loop labels

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,6 +1,21 @@
 fn main() {
-    let condition = true;
+    let mut count = 0;
 
-    let number = if condition { 5 } else { 6 };
-    println!("The value of number is: {}", number);
+    'counting_up: loop {
+        println!("{count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("{remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 10 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
+    }
+    println!("End count: {count}");
 }
