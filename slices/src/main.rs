f1675666cc78c58e4fc3f9dde4df3f70cf942a73
Omelian Levkovych <omelianai@gmail.com>
2023-12-24 19:44:27 +0100

    Slicing the whole string

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,12 +1,19 @@
 fn main() {
     let words = String::from("Hello World !");
 
-    let index_of_space: usize = first_word(&words);
-    println!("first_word: {}", index_of_space);
+    let hello = &words[0..5];
+    let world = &words[6..11];
+    let whole_word = &words[..];
 
-    for i in 0..index_of_space {
-        print!("{}", words.as_bytes()[i] as char);
-    }
+    println!("hello: {}, world: {}", hello, world);
+    println!("whole_word: {}", whole_word);
+
+    // let index_of_space: usize = first_word(&words);
+    // println!("first_word: {}", index_of_space);
+
+    // for i in 0..index_of_space {
+    //     print!("{}", words.as_bytes()[i] as char);
+    // }
 }
 
 
