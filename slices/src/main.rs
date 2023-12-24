156db5628ec3ee654e2896c11fdd48f53015f2b1
Omelian Levkovych <omelianai@gmail.com>
2023-12-24 19:47:23 +0100

    Proper first_word fn with string slices

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,30 +1,18 @@
 fn main() {
     let words = String::from("Hello World !");
 
-    let hello = &words[0..5];
-    let world = &words[6..11];
-    let whole_word = &words[..];
-
-    println!("hello: {}, world: {}", hello, world);
-    println!("whole_word: {}", whole_word);
-
-    // let index_of_space: usize = first_word(&words);
-    // println!("first_word: {}", index_of_space);
-
-    // for i in 0..index_of_space {
-    //     print!("{}", words.as_bytes()[i] as char);
-    // }
+    let first_word = first_word(&words);
+    println!("First word is {}", first_word);
 }
 
-
-fn first_word(s: &String) -> usize {
+fn first_word(s :&String) -> &str {
     let bytes = s.as_bytes();
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[..i]
         }
     }
 
-    s.len()
+    return &s[..]
 }
