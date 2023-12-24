4d2317b44e7d0325bf5e75ca9ffba21b54d02c0a
Omelian Levkovych <omelianai@gmail.com>
2023-12-24 19:13:43 +0100

    Borrowing is not ownership

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
-    let len = calculate_length(&s1);
+    let mut len = calculate_length(&s1);
+
+    println!("The length of '{s1}' is {len}.");
+
+    change(&mut s1);
+    len = calculate_length(&s1);
 
     println!("The length of '{s1}' is {len}.");
 }
 
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(", world");
 }
