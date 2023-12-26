c44ab4929de9af7045392742ac7c0c4539ff8b27
Omelian Levkovych <omelianai@gmail.com>
2023-12-26 19:44:09 +0100

    Assiciated function in struct

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,11 +5,20 @@ fn main() {
         height: 50,
     };
 
-    dbg!(&rect1);
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
 
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
 }
