0cefafa116f0bf443e4aa1e9b33c0bbcedbc13f4
Omelian Levkovych <omelianai@gmail.com>
2023-12-26 19:38:00 +0100

    Use dbg! fn

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -1,19 +1,15 @@
 fn main() {
+    let scale = 2;
     let rect1 = Rectangle {
-        width: 30,
+        width: dbg!(30 * scale),
         height: 50,
     };
 
-
-    println!("{:?}", rect1);
+    dbg!(&rect1);
 }
 
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
