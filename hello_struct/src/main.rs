527c0defd7b07f783112ffff08d0e5296aaa89ab
Omelian Levkovych <omelianai@gmail.com>
2023-12-26 19:28:02 +0100

    Struct tuples

--- hello_struct/src/main.rs
+++ hello_struct/src/main.rs
@@ -15,7 +15,11 @@ fn main() {
         String::from("username"));
 
     println!("New user email is {}", buildUser.email);
+
+    let black = Color(0, 0, 0);
 }
+
+struct Color(i32, i32, i32);
 
 fn build_user(email: String, username: String) -> User {
     User {
