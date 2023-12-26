02f3435db06c1bc159494e0edd1cdbff566a0b50
Omelian Levkovych <omelianai@gmail.com>
2023-12-26 19:22:09 +0100

    User struct

--- hello_struct/src/main.rs
+++ hello_struct/src/main.rs
@@ -1,5 +1,14 @@
 fn main() {
+    let mut user = User {
+        active: false,
+        username: String::from("John Doe"),
+        email: String::from("johndoe@example.com"),
+        sign_in_count: 1,
+    };
 
+    user.sign_in_count = 2;
+
+    println!("User sign in count is {}", user.sign_in_count);
 }
 
 struct User {
