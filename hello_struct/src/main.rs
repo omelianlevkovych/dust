ccad5838ff64e83877fa8ae03090a3462eb658c4
Omelian Levkovych <omelianai@gmail.com>
2023-12-26 19:25:20 +0100

    Field init shorthand

--- hello_struct/src/main.rs
+++ hello_struct/src/main.rs
@@ -9,6 +9,21 @@ fn main() {
     user.sign_in_count = 2;
 
     println!("User sign in count is {}", user.sign_in_count);
+
+    let buildUser = build_user(
+        String::from("email"),
+        String::from("username"));
+
+    println!("New user email is {}", buildUser.email);
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
 }
 
 struct User {
